94348576d0bc9d515d7322e6a53976802d90d7c6
Sajeev K R <A-8729@ibsplc.com>
2019-02-04 18:33:42 +0530

    Initial Commit

---
+++ java-vault/src/com/logicbrace/java8/ConsumerCustom.java
@@ -0,0 +1,9 @@
+package com.logicbrace.java8;
+
+public class ConsumerCustom {
+	
+	public static void main(String args[]) {
+		java.util.function.Consumer<String> consumer= () -> {System.out.println("")}
+	}
+
+}
